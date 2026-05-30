f5913320e0561a25a3c71ec7a9732b9f6b05c2e5
Artur Fornagieri <fornagieri.artur@gmail.com>
2026-05-29 23:40:58 -0300

    add index page

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template_string
+
+from static.index_html import index_page
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello World</p>"
+def index():
+    page = index_page()
+    return render_template_string(page)
